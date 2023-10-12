71df0f1ca1916ba18d73fcba1cc91176b1544649
Crowdbotics <team@crowdbotics.com>
2023-10-12 16:23:29 +0000

    Initial commit

---
+++ backend/portfolios4hs_43898/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for portfolios4hs_43898 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "portfolios4hs_43898.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "portfolios4hs-43898",
+  url: "https://portfolios4hs-43898.botics.co",
+  api: "https://portfolios4hs-43898.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
